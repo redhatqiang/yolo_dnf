82a03a3d21b247dda9509779345a576996eb3692
zhangsan-feng <54844110+zhangsan-feng@users.noreply.github.com>
2023-05-29 15:19:23 +0800

    Update md.py

--- md.py
+++ md.py
@@ -7,6 +7,28 @@ if __name__ == '__main__':
     
     https://gpushare.com/store          A100/40g  4块/1小时
     https://www.autodl.com/home         4090/24GB  3块 1小时
+    
+    r -- 人物
+    w -- 物品
+    m -- 门
+    g -- 怪物
+    j -- boss 奖励
+    x -- 下一个角色
+    
+    a -- 第一张地图
+    b -- 第二张地图
+    c -- 第三张地图
+    d -- 第四张地图
+    e -- 第五张地图
+    f -- 第六张地图
+    h -- boss 地图
+    
+    k -- 
+    l -- 
+    m -- 
+    
+    r-w-m-g-j-x-a-b-c-d-e-f-h-k-l-m
+    
     '''
 
     pass
