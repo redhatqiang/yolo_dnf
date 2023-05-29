6af30b291afcfe36405b7ad71b57f949c3db99d0
zhangsan-feng <54844110+zhangsan-feng@users.noreply.github.com>
2023-05-29 14:17:25 +0800

    Update md.py

--- md.py
+++ md.py
@@ -4,10 +4,9 @@
 if __name__ == '__main__':
 
     '''
+    
     https://gpushare.com/store          A100/40g  4块/1小时
     https://www.autodl.com/home         4090/24GB  3块 1小时
     '''
-
-
 
     pass
