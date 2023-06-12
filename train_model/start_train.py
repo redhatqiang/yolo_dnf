0b63173c56c6dcc25c4334e4cf33a4a723a33908
往昔流年 <54844110+zhangsan-feng@users.noreply.github.com>
2023-06-12 21:47:22 +0800

    1

--- train_model/start_train.py
+++ train_model/start_train.py
@@ -15,15 +15,15 @@ def StartRepeatTrainYOLOV8():
     workers = 4
     batch = 20
 
-    # print("train start first train ")
-    # model = 'yolov8n.pt'
-    # args = dict(model=model,data=data,device=device,cache=cache,workers=workers,batch=batch) # batch=16  加大这个参数提升gpu使用率
-    # YOLO(model).train(**args)
-    # print(" first train success")
+    print("train start first train ")
+    model = 'yolov8n.pt'
+    args = dict(model=model,data=data,device=device,cache=cache,workers=workers,batch=batch) # batch=16  加大这个参数提升gpu使用率
+    YOLO(model).train(**args)
+    print(" first train success")
 
-    # for i in range(5, 0, -1):
-    #     print(i, "s after start next train")
-    #     time.sleep(1)
+    for i in range(5, 0, -1):
+        print(i, "s after start next train")
+        time.sleep(1)
 
     # print("start second train")
     # model = './runs/detect/train/weights/best.pt'
@@ -35,17 +35,17 @@ def StartRepeatTrainYOLOV8():
     #     print(i, "s after start next train")
     #     time.sleep(1)
 
-    for num in range(4, 6):
-        print("start ", num + 1 ,"train")
-        model = './runs/detect/train' + str(num) + '/weights/best.pt'
-        if not os.path.isfile(model):
-            continue
-        YOLO(model).train(model=model,data=data,device=device,cache=cache,workers=workers,batch=batch)
-        print( num + 1, "train success")
-
-        for i in range(5, 0, -1):
-            print(i, "s after start next train")
-            time.sleep(1)
+    # for num in range(4, 6):
+    #     print("start ", num + 1 ,"train")
+    #     model = './runs/detect/train' + str(num) + '/weights/best.pt'
+    #     if not os.path.isfile(model):
+    #         continue
+    #     YOLO(model).train(model=model,data=data,device=device,cache=cache,workers=workers,batch=batch)
+    #     print( num + 1, "train success")
+    #
+    #     for i in range(5, 0, -1):
+    #         print(i, "s after start next train")
+    #         time.sleep(1)
 
 
 
